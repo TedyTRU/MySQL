a9e0954d5ed744e1de984f01adc6ee32cd25f521
Teodora Uzunova <tedyuzunova@gmail.com>
2022-01-17 15:48:26 +0200

    addition to 06_Exercise

--- 06_Exercise Basic CRUD.sql
+++ 06_Exercise Basic CRUD.sql
@@ -22,8 +22,8 @@ SELECT CONCAT(first_name, '.', last_name, '@softuni.bg') AS 'full_email_address'
 FROM employees;
 
 #6
-SELECT DISTINCT salary FROM employees
-ORDER BY employee_id;
+SELECT DISTINCT salary FROM employees;
+-- ORDER BY employee_id;
 
 #7
 SELECT * FROM employees
@@ -33,6 +33,7 @@ ORDER BY employee_id;
 #8
 SELECT first_name, last_name, job_title FROM employees
 WHERE salary BETWEEN 20000 AND 30000
+-- WHERE `salary` >= 20000 AND `salary` <= 30000
 ORDER BY employee_id;
 
 #9
@@ -60,6 +61,77 @@ LIMIT 5;
 SELECT first_name, last_name FROM employees
 WHERE NOT (department_id = 4);
 
+SELECT `first_name`, `last_name` FROM employees
+WHERE `department_id` != 4;
+
 #14
 SELECT * FROM employees
 ORDER BY salary DESC, first_name, last_name DESC, middle_name, employee_id;
+
+#15
+CREATE VIEW `v_employees_salaries` AS
+SELECT first_name, last_name, salary FROM employees;
+
+#16
+CREATE VIEW `v_employees_job_titles` AS 
+SELECT CONCAT_WS(' ', first_name, middle_name, last_name) AS 'full_name', job_title FROM employees;
+
+SELECT * FROM v_employees_job_titles;
+
+#17
+SELECT DISTINCT job_title FROM employees
+ORDER BY job_title;
+
+#18
+SELECT * FROM projects
+ORDER BY start_date, `name`, project_id
+LIMIT 10;
+
+#19
+SELECT first_name, last_name, hire_date FROM employees
+ORDER BY hire_date DESC
+LIMIT 7;
+
+#20
+SELECT * FROM departments;
+SELECT * FROM employees
+WHERE department_id IN (1, 2, 4, 11);
+
+
+UPDATE employees
+SET salary = salary * 1.12
+WHERE department_id IN (1, 2, 4, 11);
+SELECT salary FROM employees;
+
+#21
+SELECT peak_name FROM peaks
+ORDER BY peak_name;
+
+#22
+SELECT * FROM continents;
+
+SELECT country_name, population FROM countries
+WHERE continent_code = 'EU'
+ORDER BY population DESC, country_name
+LIMIT 30;
+
+#23
+SELECT country_name, country_code, currency_code FROM countries
+ORDER BY country_name;
+
+
+SELECT country_name, country_code, 
+CASE
+WHEN currency_code = 'EUR' THEN 'Euro'
+ELSE 'Not Euro'
+END 
+
+AS 'currency'
+FROM countries
+ORDER BY country_name;
+
+#24
+SELECT `name` FROM characters
+ORDER BY `name`;
+
+

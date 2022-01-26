01d8b639ebeddd76f65793a8f6847abc26187500
unknown <tedyuzunova@gmail.com>
2022-01-26 12:51:37 +0200

    addition to exercise_12

--- 12_Exercise Table Relations.sql
+++ 12_Exercise Table Relations.sql
@@ -133,14 +133,124 @@ SELECT * FROM teachers;
 ALTER TABLE teachers AUTO_INCREMENT = 200;
 -----
 
+
 #5
+CREATE TABLE cities (
+	`city_id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(50)
+);
+
+CREATE TABLE customers (
+	`customer_id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(50),
+    `birthday` DATE,
+    `city_id` INT,
+CONSTRAINT fk_customers_cities
+FOREIGN KEY (`city_id`)
+REFERENCES `cities` (`city_id`)
+);
+
+CREATE TABLE orders (
+	`order_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `customer_id` INT,
+CONSTRAINT fk_orders_customers
+FOREIGN KEY (`customer_id`)
+REFERENCES `customers` (`customer_id`)
+);
+
+CREATE TABLE item_types (
+	`item_type_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50)
+);
+
+CREATE TABLE items (
+	`item_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50),
+    `item_type_id` INT,
+CONSTRAINT fk_items_item_types
+FOREIGN KEY (`item_type_id`)
+REFERENCES `item_types` (`item_type_id`)
+);
+
+CREATE TABLE order_items (
+	`order_id` INT,
+    `item_id` INT,
+    
+CONSTRAINT pk_order_items
+PRIMARY KEY (`order_id`, `item_id`),
+    
+CONSTRAINT fk_order_items_orders
+FOREIGN KEY (`order_id`)
+REFERENCES `orders` (`order_id`),
+
+CONSTRAINT fk_order_items_items
+FOREIGN KEY (`item_id`)
+REFERENCES `items` (`item_id`)
+);
+
+#6
+CREATE TABLE majors (
+	`major_id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	`name` VARCHAR(50)
+);
+
+CREATE TABLE students (
+	`student_id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `student_number` VARCHAR(12),
+    `student_name` VARCHAR(50),
+    `major_id` INT,
+    
+CONSTRAINT fk_students_majors
+FOREIGN KEY (`major_id`)
+REFERENCES `majors` (`major_id`)
+);
+
+CREATE TABLE payments (
+	`payment_id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `payment_date` DATE,
+    `payment_amount` DECIMAL(8, 2),
+    `student_id` INT,
+    
+CONSTRAINT fk_payments_students
+FOREIGN KEY (`student_id`)
+REFERENCES `students` (`student_id`)
+);
+
+CREATE TABLE subjects (
+	`subject_id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `subject_name` VARCHAR(50)
+);
+
+CREATE TABLE agenda (
+	`student_id` INT,
+	`subject_id` INT,
+    
+CONSTRAINT pk_agenda
+PRIMARY KEY (`student_id`, `subject_id`),
+
+CONSTRAINT fk_agenda_students
+FOREIGN KEY (`student_id`)
+REFERENCES students (`student_id`),
+
+CONSTRAINT fk_agenda_subject
+FOREIGN KEY (`subject_id`)
+REFERENCES subjects (`subject_id`)
+);
+
+#9
+SELECT * FROM peaks;
+SELECT * FROM mountains;
+
+SELECT `mountain_range`, `peak_name`, `elevation` AS 'peak_elevation'
+FROM mountains, peaks
+WHERE `mountain_range` = 'Rila' AND `mountain_id` = 17
+ORDER BY peak_elevation DESC;
 
 
-
-
-
-
-
-
-
+SELECT m.mountain_range, p.peak_name, p.elevation AS 'peak_elevation'
+FROM peaks AS p
+JOIN mountains AS m
+ON m.id = p.mountain_id
+WHERE m.mountain_range = 'Rila' AND p.mountain_id = '17'
+ORDER BY p.elevation DESC;
  
